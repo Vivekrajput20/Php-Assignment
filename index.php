d823f8970e80c00944f3e25f786ebfb30dcdb69f
First Name <you@example.com>
2018-03-16 02:13:43 +0530

    created index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html> 
+<meta charset="utf-8">
+<head>
+  <title>Homepage</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+ <nav class="navbar navbar-inverse">
+   <div class="container-fluid">
+       <div class="navbar-header">
+             <a class="navbar-brand" href="#">WebSiteName</a>
+                 </div>
+                               <ul class="nav navbar-nav navbar-right">
+                               <li><label><input type="text" placeholder="Username" class="form-control" id="usr" ></li>
+                               <li><label><input type="password" placeholder="Password" class="form-control" id="pwd" ></li>
+                               <li><label><input type="checkbox" value="1">Remember Me</label></li>
+                                <li><button type="submit" class="btn btn-primary">Login</button></li>
+                                </ul>
+                                  </div>
+</nav>
+<div class="row">
+<div class="col-sm-7"> </div>
+<div class="col-sm-5"> </div>
+</div>
+</body>
+</html>
